a1f7b4482d69ced933bafd9173942378f285b20a
Sonali_167 <cse.22bcsf70@silicon.ac.in>
2025-08-12 12:23:10 +0530

    Some new Features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import { Navbar } from "../components/Navbar"
-import { ThemeToggle } from "../components/ThemeToggle"
 import { HeroSection } from "../components/HeroSection"
 import { AboutSection } from "../components/AboutSection"
 import { SkillsSection } from "../components/SkillsSection"
+import { ProjectSection } from "../components/ProjectSection"
+import { ContactSection } from "../components/ContactSection"
+import { Footer } from "../components/Footer"
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        {/* theme toggle */}
-        <ThemeToggle />
+=
 
 
         {/* bg  */}
@@ -22,12 +23,15 @@ export const Home = () => {
           <HeroSection />
           <AboutSection />
           <SkillsSection />
+          <ProjectSection />
+          <ContactSection /> 
 
         </main>
 
 
         
         {/* footer */}
+        <Footer />
     </div>
   )
 }
